7622bfb4bc81302feca22dc7464aaf74e8f4aa2e
Itamar Medeiros <itamarjunior@outlook.com.br>
2024-09-14 15:54:02 -0300

    third commit

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -2,8 +2,8 @@ const Table = () =>{
   return(
 
     <>
-      <img src="../assets/images/tabela-imc.png" alt="tabela imc até 60 anos" />
-      <img src="../assets/images/tabela-imc-60.png" alt="tabela imc acima de 60 anos" />
+      <img src="assets/images/tabela-imc.png" alt="tabela imc até 60 anos" />
+      <img src="assets/images/tabela-imc-60.png" alt="tabela imc acima de 60 anos" />
     </>
   )
 }
